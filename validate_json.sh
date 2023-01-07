21f694827d7a46a58fd6d9c960a8a67ede12d11a
pmb59 <pmb59@users.noreply.github.com>
2023-01-07 01:08:56 +0000

    [auto] ci: apply beautysh changes

--- validate_json.sh
+++ validate_json.sh
@@ -2,20 +2,20 @@
 
 # Check if a file has been provided as an argument
 if [ $# -eq 0 ]; then
-  echo "Error: No file provided."
-  exit 1
+    echo "Error: No file provided."
+    exit 1
 fi
 
 # Check if the file exists
 if [ ! -f "$1" ]; then
-  echo "Error: File does not exist."
-  exit 1
+    echo "Error: File does not exist."
+    exit 1
 fi
 
 # Check if the file is a valid JSON file
 if ! jq -e . "$1" > /dev/null 2>&1; then
-  echo "Error: Not a valid JSON file."
-  exit 1
+    echo "Error: Not a valid JSON file."
+    exit 1
 fi
 
 echo "Valid JSON file."
--- validate_jsonl.sh
+++ validate_jsonl.sh
@@ -4,22 +4,22 @@
 
 # Check if a file has been provided as an argument
 if [ $# -eq 0 ]; then
-  echo "Error: No file provided."
-  exit 1
+    echo "Error: No file provided."
+    exit 1
 fi
 
 # Check if the file exists
 if [ ! -f "$1" ]; then
-  echo "Error: File does not exist."
-  exit 1
+    echo "Error: File does not exist."
+    exit 1
 fi
 
 # Check if the file is a valid JSONL file
 while read -r line; do
-  if ! jq -e . <<< "$line" > /dev/null 2>&1; then
-    echo "Error: Not a valid JSONL file."
-    exit 1
-  fi
+    if ! jq -e . <<< "$line" > /dev/null 2>&1; then
+        echo "Error: Not a valid JSONL file."
+        exit 1
+    fi
 done < "$1"
 
 echo "Valid JSONL file."
